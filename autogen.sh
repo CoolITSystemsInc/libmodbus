31c5225efc9548ceade3e8f921404cc3d2a25ae2
Renee Cousins <nonarkitten@gmail.com>
2021-02-26 14:18:40 -0700

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
-if autoreconf --install --symlink --force; then
+do_symlink=
+if ln -s xxx xxx-link > /dev/null 2>&1; then
+    do_symlink=--symlink
+    rm xxx-link
+fi
+# clean up
+if autoreconf --install $do_symlink --force; then
 	echo
 	echo "------------------------------------------------------"
 	echo "Initialized build system. You can now run ./configure "
